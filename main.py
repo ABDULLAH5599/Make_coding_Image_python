bd8a544e9cbf249d60d5aa04ca2768e33c3bbe5e
ABDULLAH5599 <abdullahalmamunshifat7@gmail.com>
2022-04-18 14:55:01 +0600

    python pywhatkit

---
+++ main.py
@@ -0,0 +1,34 @@
+import pywhatkit as pw
+
+#this is image to text
+pw.image_to_ascii_art('srk-1.jpg' , 'shahrukh1.txt')
+
+
+# try:
+#
+#     txt = """hey i miss u so much
+#     my name is abdullah al mamun shifat"""
+#     pw.text_to_handwriting(txt,"demo1.png",[0,0,138])
+#     print("end")
+#
+# except:
+#
+#     # printing the error message
+#     print("Network Error Occured")
+
+# pw.sendwhats_image("AB123CDEFGHijklmn", "Images/Hello.png", "Hello")
+
+# try:
+#
+#     # sending message to receiver
+#     # using pywhatkit
+#     # pw.sendwhatmsg("+8801621453580","Hello from GeeksforGeeks",22, 28)
+#     # pw.sendwhatmsg("+8801990330987", "Hi", 00, 15, 2, True, 2)
+#     # print("Successfully Sent!")
+#     pw.playonyt("PyWhatKit")
+#
+# except:
+#
+#     # handling exception
+#     # and printing error message
+#     print("An Unexpected Error!")
